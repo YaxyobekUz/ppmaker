e16798512ec08d9c161e886a99276d0af8e53f1b
yaxyobekb <pubgn9642@gmail.com>
2024-07-03 15:27:15 +0500

    add header nav link active styles

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -3,12 +3,13 @@ import { Outlet } from "react-router";
 
 // components
 import Top from "../components/Top";
+import Header from "../components/Header";
 
 const MainLayout = () => {
   return (
     <>
       <Top />
-
+      <Header />
       <Outlet />
     </>
   );
