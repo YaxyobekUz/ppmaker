b4a7a3c9f1aeefa99de7890cd1378c32d6c03f0f
yaxyobekb <pubgn9642@gmail.com>
2024-07-04 12:03:51 +0500

    add screens config

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,6 +19,22 @@ export default {
       fontFamily: {
         Poppins: ["Poppins", "sans-serif"],
       },
+
+      zIndex: {
+        1: 1,
+        2: 2,
+        3: 3,
+        4: 4,
+        5: 5,
+      },
+    },
+
+    screens: {
+      xl: "1240px",
+      lg: "1024px",
+      md: "768px",
+      sm: "640px",
+      xs: "425px",
     },
   },
   plugins: [],
