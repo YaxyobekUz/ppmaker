59012b900a1c8c6477e28fb5a78f15183a7820b1
yaxyobekb <pubgn9642@gmail.com>
2024-07-07 11:53:33 +0500

    add services data

--- src/data/main.js
+++ src/data/main.js
@@ -1,4 +1,8 @@
 import heroSlideBg0 from "../assets/images/others/hero-bg.jpg";
+import toonBoyBg from "../assets/images/others/toon-boy-bg.jpg";
+import toonGirlBg from "../assets/images/others/toon-girl-bg.jpg";
+import specialProfilePictureBg from "../assets/images/others/special-profile-picture.jpg";
+import toonPubgGameCharactersBg from "../assets/images/others/toon-pubg-game-characters-bg.jpg";
 
 export const homepageHeroSlides = [
   {
@@ -12,5 +16,48 @@ export const homepageHeroSlides = [
     title: "pictures for girls",
     link: "/services/create/profile-picture/for-girls/1",
     image: heroSlideBg0,
-  }
+  },
+];
+
+export const services = [
+  {
+    id: 0,
+    name: "cartoon cute boy image",
+    title: "Bollar uchun profil rasmini yaratish",
+    link: "/services/create/profile-picture/for-boys/1",
+    numberOfImages: 0,
+    image: toonBoyBg,
+  },
+  {
+    id: 1,
+    name: "cartoon cute girl image",
+    title: "Qizlar uchun profil rasmini yaratish",
+    link: "/services/create/profile-picture/for-girls/1",
+    numberOfImages: 0,
+    image: toonGirlBg,
+  },
+  {
+    id: 2,
+    name: "cartoon pubg game characters image",
+    title: "O'yinlar uchun profil rasmini yaratish",
+    link: "/services/create/profile-picture/for-games/1",
+    numberOfImages: 0,
+    image: toonPubgGameCharactersBg,
+  },
+  {
+    id: 3,
+    name: "create special profile image",
+    title: "Maxsus profil rasmini yaratish",
+    link: "/services/create/special-profile-picture",
+    numberOfImages: 0,
+    image: specialProfilePictureBg,
+  },
+  {
+    id: 4,
+    name: "create favicon image",
+    title: "Veb saytlar uchun favikon yaratish",
+    link: "/services/create/favicon",
+    numberOfImages: 0,
+    image: heroSlideBg0,
+  },
 ];
