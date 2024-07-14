ec59130e39ae15e9a305be0d52870993bde243ea
yaxyobekb <pubgn9642@gmail.com>
2024-07-14 17:10:05 +0500

    add image card colors and profile images some data

---
+++ src/data/badge-colors.js
@@ -0,0 +1,28 @@
+// data
+export const badgeColorsData = [
+  {
+    value: "top",
+    background: "#7A08FA",
+    color: "#ffffff",
+  },
+  {
+    value: "premium",
+    background: "linear-gradient(90deg, #ffd60a, #faa307)",
+    color: "#424769",
+  },
+  {
+    value: "trending",
+    background: "linear-gradient(90deg, #e85d04, #faa307)",
+    color: "#ffffff",
+  },
+  {
+    value: "wonderful",
+    background: "linear-gradient(90deg, #6a994e, #a7c957)",
+    color: "#ffffff",
+  },
+  {
+    value: "normal",
+    background: "#ffffff",
+    color: "#424769",
+  },
+];
---
+++ src/data/profile-images-for-boys.js
@@ -0,0 +1,253 @@
+import teshavoyImg from "../assets/images/profile-images-for-boys/cute-cartoon-boy.jpg";
+
+// data
+export const profileImagesForBoysData = [
+  {
+    id: 0,
+    title: "Teshavoy chiroyli bola",
+    image: {
+      name: "Teshavoy is a beautiful boy",
+      template: {
+        src: teshavoyImg,
+      },
+      original: {
+        src: teshavoyImg,
+      },
+      text: {
+        body: "Ismingiz",
+        color: "white",
+        size: "medium",
+        position: "bottom-center",
+        effects: null,
+      },
+    },
+    hashtags: [
+      { id: 0, label: "teshavoy", value: "teshavoy" },
+      { id: 1, label: "multik", value: "cartoon" },
+      { id: 2, label: "chiroyli", value: "beautiful" },
+      { id: 3, label: "bola", value: "boy" },
+    ],
+    badges: [
+      { id: 0, label: "Ajoyib", value: "wonderful" },
+      { id: 1, label: "Trendda", value: "Trending" },
+    ],
+    isArchived: false,
+  },
+  {
+    id: 1,
+    title: "Teshavoy chiroyli bola",
+    image: {
+      name: "Teshavoy is a beautiful boy",
+      template: {
+        src: teshavoyImg,
+      },
+      original: {
+        src: teshavoyImg,
+      },
+      text: {
+        body: "Ismingiz",
+        color: "white",
+        size: "medium",
+        position: "bottom-center",
+        effects: null,
+      },
+    },
+    hashtags: [
+      { id: 0, label: "teshavoy", value: "teshavoy" },
+      { id: 1, label: "multik", value: "cartoon" },
+      { id: 2, label: "chiroyli", value: "beautiful" },
+      { id: 3, label: "bola", value: "boy" },
+    ],
+    badges: [
+      { id: 0, label: "Yaxshi", value: "normal" },
+      { id: 1, label: "Trendda", value: "Trending" },
+    ],
+    isArchived: false,
+  },
+  {
+    id: 2,
+    title: "Teshavoy chiroyli bola",
+    image: {
+      name: "Teshavoy is a beautiful boy",
+      template: {
+        src: teshavoyImg,
+      },
+      original: {
+        src: teshavoyImg,
+      },
+      text: {
+        body: "Ismingiz",
+        color: "white",
+        size: "medium",
+        position: "bottom-center",
+        effects: null,
+      },
+    },
+    hashtags: [
+      { id: 0, label: "teshavoy", value: "teshavoy" },
+      { id: 1, label: "multik", value: "cartoon" },
+      { id: 2, label: "chiroyli", value: "beautiful" },
+      { id: 3, label: "bola", value: "boy" },
+    ],
+    badges: [
+      { id: 0, label: "Yaxshi", value: "normal" },
+      { id: 1, label: "Trendda", value: "Trending" },
+    ],
+    isArchived: false,
+  },
+  {
+    id: 3,
+    title: "Teshavoy chiroyli bola",
+    image: {
+      name: "Teshavoy is a beautiful boy",
+      template: {
+        src: teshavoyImg,
+      },
+      original: {
+        src: teshavoyImg,
+      },
+      text: {
+        body: "Ismingiz",
+        color: "white",
+        size: "medium",
+        position: "bottom-center",
+        effects: null,
+      },
+    },
+    hashtags: [
+      { id: 0, label: "teshavoy", value: "teshavoy" },
+      { id: 1, label: "multik", value: "cartoon" },
+      { id: 2, label: "chiroyli", value: "beautiful" },
+      { id: 3, label: "bola", value: "boy" },
+    ],
+    badges: [
+      { id: 0, label: "Premium", value: "premium" },
+      { id: 1, label: "O'rta", value: "normal" },
+    ],
+    isArchived: false,
+  },
+  {
+    id: 4,
+    title: "Teshavoy chiroyli bola",
+    image: {
+      name: "Teshavoy is a beautiful boy",
+      template: {
+        src: teshavoyImg,
+      },
+      original: {
+        src: teshavoyImg,
+      },
+      text: {
+        body: "Ismingiz",
+        color: "white",
+        size: "medium",
+        position: "bottom-center",
+        effects: null,
+      },
+    },
+    hashtags: [
+      { id: 0, label: "teshavoy", value: "teshavoy" },
+      { id: 1, label: "multik", value: "cartoon" },
+      { id: 2, label: "chiroyli", value: "beautiful" },
+      { id: 3, label: "bola", value: "boy" },
+    ],
+    badges: [
+      { id: 0, label: "Top", value: "top" },
+      { id: 1, label: "Trendda", value: "trending" },
+    ],
+    isArchived: false,
+  },
+  {
+    id: 5,
+    title: "Teshavoy chiroyli bola",
+    image: {
+      name: "Teshavoy is a beautiful boy",
+      template: {
+        src: teshavoyImg,
+      },
+      original: {
+        src: teshavoyImg,
+      },
+      text: {
+        body: "Ismingiz",
+        color: "white",
+        size: "medium",
+        position: "bottom-center",
+        effects: null,
+      },
+    },
+    hashtags: [
+      { id: 0, label: "teshavoy", value: "teshavoy" },
+      { id: 1, label: "multik", value: "cartoon" },
+      { id: 2, label: "chiroyli", value: "beautiful" },
+      { id: 3, label: "bola", value: "boy" },
+    ],
+    badges: [
+      { id: 0, label: "Yaxshi", value: "normal" },
+      { id: 1, label: "Trendda", value: "Trending" },
+    ],
+    isArchived: false,
+  },
+  {
+    id: 6,
+    title: "Teshavoy chiroyli bola",
+    image: {
+      name: "Teshavoy is a beautiful boy",
+      template: {
+        src: teshavoyImg,
+      },
+      original: {
+        src: teshavoyImg,
+      },
+      text: {
+        body: "Ismingiz",
+        color: "white",
+        size: "medium",
+        position: "bottom-center",
+        effects: null,
+      },
+    },
+    hashtags: [
+      { id: 0, label: "teshavoy", value: "teshavoy" },
+      { id: 1, label: "multik", value: "cartoon" },
+      { id: 2, label: "chiroyli", value: "beautiful" },
+      { id: 3, label: "bola", value: "boy" },
+    ],
+    badges: [
+      { id: 0, label: "Yaxshi", value: "normal" },
+      { id: 1, label: "Trendda", value: "Trending" },
+    ],
+    isArchived: false,
+  },
+  {
+    id: 7,
+    title: "Teshavoy chiroyli bola",
+    image: {
+      name: "Teshavoy is a beautiful boy",
+      template: {
+        src: teshavoyImg,
+      },
+      original: {
+        src: teshavoyImg,
+      },
+      text: {
+        body: "Ismingiz",
+        color: "white",
+        size: "medium",
+        position: "bottom-center",
+        effects: null,
+      },
+    },
+    hashtags: [
+      { id: 0, label: "teshavoy", value: "teshavoy" },
+      { id: 1, label: "multik", value: "cartoon" },
+      { id: 2, label: "chiroyli", value: "beautiful" },
+      { id: 3, label: "bola", value: "boy" },
+    ],
+    badges: [
+      { id: 0, label: "Yaxshi", value: "normal" },
+      { id: 1, label: "Trendda", value: "Trending" },
+    ],
+    isArchived: false,
+  },
+];
