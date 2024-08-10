0cb15a045daf4ccfa8509f9c34b1aa0218d570ec
yaxyobekb <pubgn9642@gmail.com>
2024-08-10 14:17:32 +0500

    add scroll to top

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Outlet } from "react-router";
+import React, { useEffect } from "react";
+import { Outlet, useLocation } from "react-router";
 
 // toaster
 import { Toaster } from "react-hot-toast";
@@ -10,6 +10,12 @@ import Header from "../components/Header";
 import Footer from "../components/Footer";
 
 const MainLayout = () => {
+  // scroll to top
+  const location = useLocation();
+  useEffect(() => {
+    window.scrollTo({ top: 0, left: 0,behavior:"smooth" });
+  }, [location]);
+
   return (
     <div className="flex flex-col w-full min-h-screen">
       <Top />
