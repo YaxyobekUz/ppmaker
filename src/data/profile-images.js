2458c5e57acf0060d600e0ef56377e8fc80b5d4a
yaxyobekb <pubgn9642@gmail.com>
2024-08-11 17:04:19 +0500

    add profile images data file

---
+++ src/data/profile-images.js
@@ -0,0 +1,35 @@
+import teshavoyImg from "../assets/images/profile-images-for-boys/cute-cartoon-boy.jpg";
+
+export const profileImagesData = [
+  {
+    id: 0,
+    type: "for-boys",
+    title: "Teshavoy chiroyli bola",
+    image: {
+      name: "Teshavoy is a beautiful boy",
+      template: {
+        src: teshavoyImg,
+      },
+      original: {
+        src: teshavoyImg,
+      },
+    },
+    text: {
+      body: "Ismingiz",
+      color: "white",
+      size: "medium",
+      position: "bottom-center",
+      effects: null,
+    },
+    hashtags: [
+      { id: 0, label: "teshavoy", value: "teshavoy" },
+      { id: 1, label: "multik", value: "cartoon" },
+      { id: 2, label: "chiroyli", value: "beautiful" },
+      { id: 3, label: "bola", value: "boy" },
+    ],
+    badges: [
+      { id: 0, label: "Ajoyib", value: "wonderful" },
+      { id: 1, label: "Trendda", value: "trending" },
+    ],
+  },
+];
